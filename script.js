157320f26e54ed4a84b04edcd4282c2dafe9bf7a
tamseel11 <kingtamseelcena@gmail.com>
2024-09-14 17:33:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Here you can use AJAX to send form data to your server or use a service like Formspree
+
+    alert('Thank you for your message! We will get back to you soon.');
+});
+
